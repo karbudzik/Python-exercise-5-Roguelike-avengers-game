9b6b0d291d6ce4cfcdc803d204a0bc5f3858dae0
karbudzik <karolinabudzik92@gmail.com>
2020-01-16 14:50:32 +0100

    Add create_player() function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import util
 import engine
 import ui
+import random
+import sys
 
 boards = {
     "board_1": {
@@ -43,21 +45,14 @@ boards = {
         }
     }
 }
-player= {
-    "position": {
-        "x": 5,
-        "y": 5,
-    },
+# player= {
+#     "position": {
+#         "x": 5,
+#         "y": 5,
+#     },
 
-    "icon": "@"
-}
-
-PLAYER_ICON = '@'
-PLAYER_START_X = 3
-PLAYER_START_Y = 3
-
-INITIAL_BOARD = boards["board_1"]
-
+#     "icon": "@"
+# }
 
 def create_player():
     '''
@@ -68,13 +63,44 @@ def create_player():
     dictionary
     '''
 
-    pass
+    user_name, user_icon = ask_for_details()
+    player = {
+        "name": user_name,
+        "position_x": 3,
+        "position_y": 3,
+        "current_board": "board_1",
+        "player_icon": user_icon
+    }
+    print(player)
+    return player
 
+def ask_for_details():
+    """
+    Asks player for name and icon.
+    
+    Returns:
+    User's details
+    """
+    possible_names = ["Captain America", "Iron Man", "Hulk", "Thor", "Black Widow", "War Machine", "Captain Marvel", "Spider-Man", "Black Panther", "Star Lord"]
+    user_name = input("Choose your name or press 'r' if you want to randomly pick a name from the Avengers team. > ")
+    if user_name in ["r", "R"] or len(user_name) < 1:
+        user_name = random.choice(possible_names)
+    
+    possible_icons = ["@", "&", "#", "%"]
+    user_icon = input("Choose the icon you want to be your character. You can choose between '@', '&', '#' and '%' or press 'r' if you want to pick randomly. > ")
+    if user_icon in ["r", "R"] or len(user_icon) < 1:
+        user_icon = random.choice(possible_icons)
+    while user_icon not in possible_icons:
+        user_icon = input("You can't pick this character. You can choose between '@', '&', '#' and '%' or press 'r' to pick randomly. > ")
+
+    return user_name, user_icon
 
 def main():
-    # nie wiem czy dobrze zrobiłem ale chwilowo zakomentowałem, bo poki co funckja nie tworzy jeszcze gracza a pewnie bedzie, ja na potrzeby mojej funkcji musiałem stworzyć obiekt player, zeby zmieniac mu korydnanty, może mam to zrobić innaczej dajcie znać to lekko zmienie dane w mojej funkcji move_player()
-    # player = create_player()
-    board = engine.create_board(INITIAL_BOARD)
+    # nie wiem czy dobrze zrobiłem ale chwilowo zakomentowałem, bo poki co funckja nie tworzy jeszcze gracza a pewnie bedzie, 
+    # ja na potrzeby mojej funkcji musiałem stworzyć obiekt player, zeby zmieniac mu korydnanty, 
+    # może mam to zrobić innaczej dajcie znać to lekko zmienie dane w mojej funkcji move_player()
+    player = create_player()
+    board = engine.create_board(boards[player["current_board"]])
     # tu jakiś dialog początkowy z userem?
     util.clear_screen()
     is_running = True
@@ -90,7 +116,7 @@ def main():
         else:
             pass
         #wylaczylem na chwile zeby nie znikaly printy
-        # util.clear_screen()
+        util.clear_screen()
 
 
 if __name__ == '__main__':
