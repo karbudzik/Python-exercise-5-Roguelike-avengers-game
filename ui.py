291de96df4f125c40436b6ed9310b286181d32a3
Mateusz Piskór <grytychy@gmail.com>
2020-01-22 11:59:24 +0100

    Just 2 lines has removed from ui.py

--- ui.py
+++ ui.py
@@ -127,7 +127,7 @@ def print_table(title, dictionary, headers, console_width):
     vertical_break = " | "
     total_width = first_width + len(vertical_break) + second_width
     print()
-    
+
     print((title).center(console_width))
     print(((total_width) * "-").center(console_width))
     print((f"{headers[0]:>{first_width}}{vertical_break}{headers[1]:>{second_width}}").center(console_width))
@@ -139,7 +139,6 @@ def print_table(title, dictionary, headers, console_width):
     print(((total_width) * "-").center(console_width))
 
 def display_logo(art):
-    # console_width = os.get_terminal_size().rows
     print("\n\n")
     for line in art:
         print("\t\t\t\t\t\t\t{}".format(line), end="")
